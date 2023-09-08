d4db5ff615020d477261f3e812ed95a41b83fd0b
AngelJ1966 <141685709+AngelJ1966@users.noreply.github.com>
2023-09-08 20:13:26 -0300

    Initial commit

---
+++ src/universidadg23/UniversidadG23.java
@@ -0,0 +1,15 @@
+
+package universidadg23;
+
+import universidadg23.accesoADatos.Conexion;
+
+
+public class UniversidadG23 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Conexion.conectarServidor();
+    }
+    
+}
